b0e230a0ef5a4c0a33a935e1982417c1ebdcd7fc
Eibelf <103249802+Eibelf@users.noreply.github.com>
2022-12-02 14:04:16 +0100

    02:12:2022 14:04

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
-
-var kurs = new Array();
+let kurs = new Array();
 kurs[0] = 1.95583;
 kurs[1] = 6.55957;
 kurs[2] = 1937.98450;
-function umrechnen(f) { //f ist eine Referenz
-                        //auf das Formular
-   var betrag = f.betrag.value;
+
+function umrechnen(f) {
+   let betrag = f.betrag.value;
    if (betrag.indexOf(",")!=-1) {
       betrag = betrag.substring(0, betrag.indexOf(","))
       + "." +
                betrag.substring(betrag.indexOf(",")+1,
                betrag.length);
    }
-   var faktor = kurs[f.Waehrung.selectedIndex];
+   let faktor = kurs[f.Waehrung.selectedIndex];
    if (f.umrechnung[0].checked) {
       f.ausgabe.value = betrag / faktor;
    } else {
       f.ausgabe.value = faktor * betrag;
    }
 }
-//--></script>
