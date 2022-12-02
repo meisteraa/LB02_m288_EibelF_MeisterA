8286d723aac36d511a3e7763b7ba08288857535e
meisteraa <meistera@bzz.ch>
2022-12-02 14:44:21 +0100

    02.12.22,14:44

---
+++ main.js
@@ -0,0 +1,21 @@
+let kurs = new Array();
+kurs[0] = 1.01492;
+kurs[1] = 1.06247;
+kurs[2] = 144.606;
+kurs[3] = 0.87265;
+
+function umrechnen(f) {
+   let betrag = f.betrag.value;
+   if (betrag.indexOf(",")!=-1) {
+      betrag = betrag.substring(0, betrag.indexOf(","))
+      + "." +
+               betrag.substring(betrag.indexOf(",")+1,
+               betrag.length);
+   }
+   let faktor = kurs[f.Waehrung.selectedIndex];
+   if (f.umrechnung[0].checked) {
+      f.ausgabe.value = betrag / faktor;
+   } else {
+      f.ausgabe.value = faktor * betrag;
+   }
+}
