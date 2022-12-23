b2dd03225255a7b4b87e41de41e8cf8464b81bfa
Eibelf <103249802+Eibelf@users.noreply.github.com>
2022-12-23 14:01:53 +0100

    14:01

--- main.js
+++ main.js
@@ -1,33 +1,53 @@
+let fileInput = document.createElement('input');
+fileInput.type = 'file';
 
 
-let meinekurse = new Array();
+fileInput.addEventListener('change', function(e) {
+  
+    let file = e.target.files[0];
 
-function loadRates() {
-    $.getJSON('daten.json', function (data) {
-        meinekurse = data;});
+    let reader = new FileReader();
 
+ 
+    reader.addEventListener('load', function(e) {
+       
+        let result = e.target.result;
+
+        jara=JSON.parse(result);
+           
+           for (i = 0; i<=3; i++){
+            kurs=jara.fxrates[i].fx;
+           name=jara.fxrates[i].name;
+           
+           var opt = document.createElement('option');
+            opt.value = kurs;
+            opt.innerHTML = name +" "+kurs;
+           document.getElementById('box').appendChild(opt);
+           }
+    });
+
+    // Datei auslesen
+    reader.readAsText(file);
+   
+   
+});
+
+// dateiberarbeitung
+document.body.appendChild(fileInput);
+text=fileInput.textContent;
+
+function berechnen(richtung)
+{
+  kurs=  document.getElementById('box').options[box.selectedIndex].value;
+  betrag=document.getElementById('betrag').value;
+  
+  if (richtung=="vonCHF"){
+    ergebnis =betrag*kurs;
+  }
+  else{
+    ergebnis =betrag/kurs;
+  }
+  //alert(richtung+" "+betrag+" "+kurs+" "+ergebnis);
+  document.getElementById('ausgabe').value=ergebnis
 }
-  //  alert(meinekurse[0].name);
-console.log(meinekurse[0]."fx")
-//kurs[0] = 1.01492
-//kurs[1] = 1.06247;
-//kurs[2] = 144.606;
-//kurs[3] = 0.87265;
 
-
-
-function umrechnen(f) {
-   let betrag = f.betrag.value;
-   if (betrag.indexOf(",")!=-1) {
-      betrag = betrag.substring(0, betrag.indexOf(","))
-      + "." +
-               betrag.substring(betrag.indexOf(",")+1,
-               betrag.length);
-   }
-   let faktor = kurs[f.Waehrung.selectedIndex];
-   if (f.umrechnung[0].checked) {
-      f.ausgabe.value = betrag / faktor;
-   } else {
-      f.ausgabe.value = faktor * betrag;
-   }
-}
